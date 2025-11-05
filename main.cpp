8302cf9536896c678158e2ba546f7d98feddd5a7
Maria Ilicheva <123975749+MariaIlicheva@users.noreply.github.com>
2025-11-06 01:07:53 +0300

    init

--- main.cpp
+++ main.cpp
@@ -1,6 +1,137 @@
 #include <fstream>
+#include <string>
+#include <iostream>
 
-int main(int argc, char* argv[]){
-  
-  return 0;
+
+std::string brainFuck(const std::string &code, const std::string &input) {
+    unsigned char tape[30000] = {0};
+    unsigned char *m = tape;   
+    std::string result;
+
+    int i = 0;
+    int ci = 0;
+
+    while (ci < code.size()) {
+        char ch = code[ci];
+
+        switch (ch) {
+            case '>':
+                ++m;
+                if (m >= tape + 30000) {
+                    return "ERROR";
+                }
+                break;
+
+            case '<':
+                if (m == tape) {
+                    return "ERROR";
+                }
+                --m;
+                break;
+
+            case '+':
+                ++(*m);
+                break;
+
+            case '-':
+                --(*m);
+                break;
+
+            case '.':
+                result += static_cast<char>(*m);
+                break;
+
+            case ',':
+                if (i >= input.size()) {
+                    return "ERROR";
+                }
+                *m = static_cast<unsigned char>(input[i++]);
+                break;
+
+            case '[':
+                if (*m == 0) {
+                    int f = 1;
+                    ++ci;
+                    while (ci < code.size() && f > 0) {
+                        if (code[ci] == '[') {
+                        	++f;
+                        }
+                        else if (code[ci] == ']') {
+                        	--f;
+                        }
+                        if (f > 0) {
+                        	++ci;
+                        }
+                    }
+                    if (f != 0) {
+                        return "ERROR"; 
+                    }
+                    break; 
+                }
+                break;
+
+            case ']':
+                if (*m != 0) {
+                    if (ci == 0) {
+                        return "ERROR";
+                    }
+                    int f = 1;
+                    --ci;
+                    while (ci < code.size() && f > 0) {
+                        if (code[ci] == ']') {
+                        	++f;
+                        }
+                        else if (code[ci] == '[') {
+                        	--f;
+                        }
+                        if (f > 0) {
+                            if (ci == 0) {
+                                return "ERROR";
+                            }
+                            --ci;
+                        }
+                    }
+                    if (f != 0) {
+                        return "ERROR";
+                    }
+                    break;
+                }
+                break;
+
+            default: break;
+        }
+
+        ++ci;
+    }
+
+    return result;
+}
+
+std::string read_file(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "! ERROR\n";
+        return 0;
+    }
+
+    file.seekg(0, std::ios::end);
+    std::streamsize sz = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    std::string in(sz, '\0');
+    if (!file.read(&in[0], sz)) {
+        std::cout << "ERROR\n";
+        std::exit(1);
+    }
+
+    return in;
+}
+
+int main(int argc, char* argv[]) {
+    std::string code = read_file(argv[1]);
+    std::string input = read_file(argv[2]);
+
+	std::cout << brainFuck(code, input);
+	
+	return 0;
 }
